b5c59e764155f0aaf7e968f090cc93495120b513
HachemiMohamedRamzi <mohammad.hachemi@flex-ops.com>
2026-02-21 10:57:00 +0400

    enhance error

--- yadouda_game_scanner/controllers/scanner.py
+++ yadouda_game_scanner/controllers/scanner.py
@@ -14,16 +14,7 @@ class YadoudaScanner(http.Controller):
         if request.env.user not in game.responsible_user_ids:
             return request.render('web.http_error', {'status_code': 403, 'status_message': 'Not authorized'})
 
-        # Required by website's frontend_layout inheritance (avoids KeyError: 'website')
-        qcontext = {
-            'game': game,
-            'main_object': game,
-        }
-        if hasattr(request, 'website') and request.website:
-            qcontext['website'] = request.website
-        elif 'website' in request.env:
-            qcontext['website'] = request.env['website'].get_current_website()
-        return request.render('yadouda_game_scanner.scanner_template', qcontext)
+        return request.render('yadouda_game_scanner.scanner_template', {'game': game})
 
     @http.route('/yadouda/scan', auth='user', type='json')
     def scan_ticket(self, game_id, ticket_code):
